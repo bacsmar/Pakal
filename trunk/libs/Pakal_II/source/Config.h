c764d2b7df42c6fbe24d4a4d558d470bdf0508e7
SNRG <SNRG@deab2f4f-25c9-4a44-aba5-3c43511eb6ab>
2015-06-08 20:50:13 +0000

    * renaming teo -> Pakal

--- trunk/libs/Pakal_II/source/Config.h
+++ trunk/libs/Pakal_II/source/Config.h
@@ -12,47 +12,36 @@
 
 #pragma once
 
-#define TEO_COMPILE_WITH_EDITOR 0
+#define PAKAL_COMPILE_WITH_EDITOR 0
 
 //! compile with LUA Scripts
-#define TEO_USE_SCRIPTS 0
+#define PAKAL_USE_SCRIPTS 0
 //! compile with ogre3D
-#define TEO_USE_OGRE 0
+#define PAKAL_USE_OGRE 0
 //! compile with Irrlicht
-#define TEO_USE_IRRLICHT 1
+#define PAKAL_USE_IRRLICHT 1
 
 //! compile with http://www.sfml-dev.org
-#define TEO_USE_SFML 0 
+#define PAKAL_USE_SFML 0 
 
 //! compile with bullet physics
-#define TEO_USE_BULLET 0
+#define PAKAL_USE_BULLET 0
 
 //! compile with BOX2D physics
-#define TEO_USE_BOX2D 1
-
-//! compile with OIS
-#define TEO_USE_OIS 0 
-
-#if COMPILE_FOR_EDITOR || defined(ANDROID) || TEO_USE_SFML
-#undef TEO_USE_OIS
-#define TEO_USE_OIS 0
-#endif
+#define PAKAL_USE_BOX2D 1
 
 //! compile with Rocket GUI
-#define TEO_USE_ROCKET 1
-
-//! compile with CAUDIO
-#define TEO_USE_CAUDIO 0
+#define PAKAL_USE_ROCKET 1
 
 //! compile with DEBUGGER
-#define TEO_USE_DEBUGGER 1
+#define PAKAL_USE_DEBUGGER 0
 
 //! compile with PROFILER
-#define TEO_USE_PROFILER 1
+#define PAKAL_USE_PROFILER 0
 
 //! compile with LOG
-#define TEO_USE_LOG 1
-#define TEO_USE_LOG_FILE 0
+#define PAKAL_USE_LOG 1
+#define PAKAL_USE_LOG_FILE 0
 
 //! compile with multithread
-#define TEO_USE_THREADS 0
+#define PAKAL_USE_THREADS 0
---
+++ trunk/libs/Pakal_II/source/Platform.h
@@ -0,0 +1,89 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2012 The PakalEngine Open Source Project
+// File: Platform.h
+// Original Author: Salvador Noel Romo Garcia.
+// Creation Date: october 30 2012
+// Specification Document:
+//
+// Purpose: Platform specific defines
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#pragma once
+//-------------- Version -------------------------------------------//
+#define PAKAL_VERSION_MAJOR 2
+#define PAKAL_VERSION_MINOR 0
+#define PAKAL_VERSION_PATCH 0
+#define PAKAL_VERSION_NAME "2.0.0"
+
+#define PAKAL_VERSION ((PAKAL_VERSION_MAJOR << 16) | (PAKAL_VERSION_MINOR << 8) | PAKAL_VERSION_PATCH)
+
+//------------------------------------------------------------------//
+
+#ifdef PAKAL_DYNAMIC_LIB
+#  undef PAKAL_DYNAMIC_LIB
+#endif
+
+#define PAKAL_DYNAMIC_LIB
+
+#define PAKAL_NONCLIENT_BUILD
+
+// Default is blank for most OS's
+#define _PAKALExport
+
+//-------------- Determine Compiler ---------------------------------
+#if defined( _MSC_VER )
+#	define PAKAL_MSVC_COMPILER
+#elif defined( __GNUC__ )
+#	if defined( __WIN32__ ) || defined( _WIN32 )
+#		define PAKAL_MINGW_COMPILER
+#	else
+#		define PAKAL_GCC_COMPILER
+#	endif
+#elif defined( __BORLANDC__ )
+#	define PAKAL_BORLAND_COMPILER
+#else
+#	error No Recognized Compiler!
+#endif
+
+// --------------- Determine Operating System Platform ---------------
+#if defined( __WIN32__ ) || defined( _WIN32 ) // Windows 2000, XP, ETC
+#	if defined ( _XBOX )
+#		define PAKAL_XBOX_PLATFORM
+#	else
+#		define PAKAL_WIN32_PLATFORM
+#		if defined( PAKAL_DYNAMIC_LIB )
+#			undef _PAKALExport
+//Ignorable Dll interface warning...
+#           if !defined(PAKAL_MINGW_COMPILER)
+#			    pragma warning (disable : 4251)
+#           endif
+#			if defined( PAKAL_NONCLIENT_BUILD )
+#				define _PAKALExport __declspec( dllexport )
+#			else
+#               if defined(PAKAL_MINGW_COMPILER)
+#                   define _PAKALExport
+#               else
+#				    define _PAKALExport __declspec( dllimport )
+#               endif
+#			endif
+#		endif
+#	endif
+#elif defined( __APPLE_CC__ ) // Apple OS X
+// Device                                       Simulator
+#   if __IPHONE_OS_VERSION_MIN_REQUIRED >= 20201 || __IPHONE_OS_VERSION_MIN_REQUIRED >= 20000
+//#   if __IPHONE_OS_VERSION_MIN_REQUIRED >= 30000 || __IPHONE_OS_VERSION_MIN_REQUIRED >= 30000
+#       define PAKAL_IPHONE_PLATFORM
+#   else
+#       define PAKAL_APPLE_PLATFORM
+#   endif
+#   undef _PAKALExport
+#   define _PAKALExport __attribute__((visibility("default")))
+#elif defined(OS_ANDROID) || defined(ANDROID)
+#	define PAKAL_ANDROID_PLATFORM
+#elif defined(__linux__)
+#	define PAKAL_LINUX_PLATFORM
+//#endif
+#endif
+
