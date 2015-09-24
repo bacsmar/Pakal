4fa00caa48e79dbe5bdc9528b1fb117333d4efe9
SNRG <SNRG@deab2f4f-25c9-4a44-aba5-3c43511eb6ab>
2015-09-24 18:48:07 +0000

    * code cleaning

--- trunk/libs/Pakal_II/source/FPSCounter.h
+++ trunk/libs/Pakal_II/source/FPSCounter.h
@@ -6,7 +6,7 @@ class FPSCounter
 {
 public:
 	FPSCounter::FPSCounter()
-	: m_fps(60), m_start_time(0), m_frames_counted(0)
+	: m_fps(60), m_elapsed_milliseconds(0), m_frames_counted(0)
 	{
 	}
 	//! returns current fps
@@ -19,24 +19,21 @@ public:
 	void register_frame(long long dt)
 	{
 		++m_frames_counted;
-		m_start_time += dt;
-		const auto milliseconds = m_start_time;
+		m_elapsed_milliseconds += dt;		
 
-		if (milliseconds >= 1500 )
+		if (m_elapsed_milliseconds >= 1500 )
 		{
-			const float invMilli = 1.f /( (float) milliseconds );
-
-			m_fps = ceil ( ( 1000 * m_frames_counted ) * invMilli );
+			m_fps = static_cast<unsigned>(ceil ( ( 1000.0 * m_frames_counted ) / m_elapsed_milliseconds));
 
 			m_frames_counted = 0;			
-			m_start_time = 0;
+			m_elapsed_milliseconds = 0;
 		}
 	}
 
 private:
 
 	unsigned m_fps;	
-	long long m_start_time;
+	long long m_elapsed_milliseconds;
 	unsigned m_frames_counted;
 };
 
