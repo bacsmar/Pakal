4838475a39af82be49eeb93aa00ff057687d8b45
SNRG <SNRG@deab2f4f-25c9-4a44-aba5-3c43511eb6ab>
2015-10-05 20:49:15 +0000

    * sori luis....

--- source/path.h
+++ source/path.h
@@ -6,43 +6,15 @@
 
 namespace Pakal
 {
-	class _PAKALExport path
-	{
-		std::string m_path;
-
+	class _PAKALExport path : public std::string
+	{		
 	public:
-		path(const char* str) : m_path(str)
+		path(const char* str) : std::string(str)
 		{
-			std::replace(m_path.begin(), m_path.end(), '\\', '/');
-			std::transform(m_path.begin(), m_path.end(), m_path.begin(), ::tolower);
-		}
-		path& operator=(const path& other)
-		{
-			if (this == &other)
-				return *this;
-			m_path = other.m_path;
-			return *this;
-		}
-		path& operator=(path&& other)
-		{
-			if (this == &other)
-				return *this;
-			m_path = std::move(other.m_path);
-			return *this;
+			std::replace(begin(), end(), '\\', '/');
+			std::transform(begin(), end(), begin(), ::tolower);
 		}
 
-		path(const std::string& str) : path(str.c_str()) {}
-		path(const path& other) : m_path { other.m_path } {}
-		path(path&& other) : m_path { std::move(other.m_path) } {}
-
-		operator const std::string&() const { return m_path; }
-		operator const char*() const { return m_path.c_str(); }
-
-		const char* c_str() const { return m_path.c_str();}
-
-		inline std::size_t size() { return m_path.size(); }
-
-		bool operator<(const path& path2) const { return m_path < path2.m_path; }
-
+		path(const std::string& str) : path(str.c_str()) {}		
 	};
 }
