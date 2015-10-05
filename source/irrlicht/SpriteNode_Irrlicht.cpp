5f74e43de0d44874728b6dd935552bf003aaf5d7
SNRG <SNRG@deab2f4f-25c9-4a44-aba5-3c43511eb6ab>
2015-10-05 04:34:51 +0000

    * refactoring SpriteIrrlicht

--- source/irrlicht/SpriteNode_Irrlicht.cpp
+++ source/irrlicht/SpriteNode_Irrlicht.cpp
@@ -8,10 +8,9 @@
 using namespace irr;
 using namespace scene;
 
-SpriteNode_Irrlicht::SpriteNode_Irrlicht(ISceneNode* parent, ISceneManager* mgr, s32 id, unsigned frameTime, bool paused, bool looped) 
+SpriteNode_Irrlicht::SpriteNode_Irrlicht(ISceneNode* parent, ISceneManager* mgr, s32 id)
 	: ISceneNode(parent,mgr, id),
-    m_sprite(nullptr), m_frameTime(frameTime), m_currentTime(0), m_currentFrame(0), m_isPaused(paused), m_isLooped(looped), m_texture(nullptr),
-	m_isFlipped(0)
+    m_texture(nullptr)
 {	
 	video::SColor scolor(255,255,255,255);			
 	m_material.Wireframe = false;
@@ -34,52 +33,6 @@ SpriteNode_Irrlicht::SpriteNode_Irrlicht(ISceneNode* parent, ISceneManager* mgr,
 	m_indices[5] = 2;		
 }
 
-void SpriteNode_Irrlicht::setAnimation(const SpriteIrrlicht& animation)
-{
-    m_sprite = &animation;
-	m_texture = m_sprite->getSpriteSheet();
-    m_currentFrame = 0;
-    setFrame(m_currentFrame);
-}
-
-void SpriteNode_Irrlicht::setAnimation(const std::string& animationName)
-{
-}
-
-void SpriteNode_Irrlicht::setFrameTime(unsigned time)
-{
-    m_frameTime = time;
-}
-
-void SpriteNode_Irrlicht::play()
-{
-    m_isPaused = false;
-}
-
-void SpriteNode_Irrlicht::play(const SpriteIrrlicht& animation)
-{
-    if (getAnimation() != &animation)
-        setAnimation(animation);
-    play();
-}
-
-void SpriteNode_Irrlicht::pause()
-{
-    m_isPaused = true;
-}
-
-void SpriteNode_Irrlicht::stop()
-{
-    m_isPaused = true;
-    m_currentFrame = 0;
-    setFrame(m_currentFrame);
-}
-
-void SpriteNode_Irrlicht::setLooped(bool looped)
-{
-    m_isLooped = looped;
-}
-
 void SpriteNode_Irrlicht::setColor(const video::SColor& color)
 {
     // Update the vertices' color
@@ -89,17 +42,15 @@ void SpriteNode_Irrlicht::setColor(const video::SColor& color)
     m_vertices[3].Color = color;
 }
 
-const SpriteIrrlicht* SpriteNode_Irrlicht::getAnimation() const
+void SpriteNode_Irrlicht::set_texture(irr::video::ITexture* texture)
 {
-    return m_sprite;
+	m_texture = texture;
 }
 
 irr::core::rectf SpriteNode_Irrlicht::getLocalBounds() const
-{
-    irr::core::recti rect = m_sprite->getFrame(m_currentFrame);
-
-    float width = static_cast<float>(std::abs(rect.getWidth()));
-    float height = static_cast<float>(std::abs(rect.getHeight()));
+{	
+    float width = static_cast<float>(std::abs(m_frame_rect.getWidth()));
+    float height = static_cast<float>(std::abs(m_frame_rect.getHeight()));
 
     return irr::core::rectf(0.f, 0.f, width, height);
 }
@@ -114,38 +65,24 @@ const core::aabbox3d<f32>& SpriteNode_Irrlicht::getBoundingBox() const
 	return m_box;
 }
 
-bool SpriteNode_Irrlicht::isLooped() const
-{
-    return m_isLooped;
-}
-
-bool SpriteNode_Irrlicht::isPlaying() const
-{
-    return !m_isPaused;
-}
-
-unsigned SpriteNode_Irrlicht::getFrameTime() const
-{
-    return m_frameTime;
-}
-
 inline core::vector3df vector2Dto3D(const core::vector2df &v2d)
 {
 	return core::vector3df(v2d.X, v2d.Y, 0.0f);
 }
 
-void SpriteNode_Irrlicht::setFrame(std::size_t newFrame, bool resetTime)
+void SpriteNode_Irrlicht::setFrame(std::size_t frameIndex, const SpriteIrrlicht* sprite)
 {
-    if (m_sprite)
+    if (sprite)
     {
         //calculate new vertex positions and texture coordiantes
-        irr::core::recti	rect = m_sprite->getFrame(newFrame);
-		core::vector2di		offset_i (m_sprite->getOffset(newFrame));
+        irr::core::recti	rect = sprite->getFrame(frameIndex);
+		m_frame_rect = rect;
+		core::vector2di		offset_i (sprite->getOffset(frameIndex));
 		core::vector2df		offset;
 		offset.X = (irr::f32)offset_i.X;
 		offset.Y = (irr::f32)offset_i.Y;
 
-		core::dimension2du d = m_sprite->getSpriteSheet()->getSize();		
+		core::dimension2du d = sprite->getSpriteSheet()->getSize();
 
 		auto height = rect.LowerRightCorner.Y;
 		auto width = rect.LowerRightCorner.X;		
@@ -175,47 +112,7 @@ void SpriteNode_Irrlicht::setFrame(std::size_t newFrame, bool resetTime)
 			m_box.addInternalPoint(m_vertices[i].Pos);
 
 		m_material.setTexture(0, m_texture);
-    }
-
-    if (resetTime)
-        m_currentTime = 0;
-}
-
-void SpriteNode_Irrlicht::update(unsigned deltaTime)
-{
-    // if not paused and we have a valid animation
-    if (!m_isPaused && m_sprite)
-    {
-        // add delta time
-        m_currentTime += deltaTime;
-
-        // if current time is bigger then the frame time advance one frame
-        if (m_currentTime >= m_frameTime)
-        {
-            // reset time, but keep the remainder
-            //m_currentTime = sf::microseconds(m_currentTime.asMicroseconds() % m_frameTime.asMicroseconds());
-			m_currentTime = ((int)(m_currentTime*1000) % (int)(m_frameTime));
-
-            // get next Frame index
-            if (m_currentFrame + 1 < m_sprite->getSize())
-            {
-				++m_currentFrame;
-            }                
-            else
-            {
-                // animation has ended
-                m_currentFrame = 0; // reset to start
-
-                if (!m_isLooped)
-                {
-                    m_isPaused = true;
-                }
-            }
-
-            // set the current frame, not reseting the time
-            setFrame(m_currentFrame, false);
-        }
-    }
+    }    
 }
 
 void SpriteNode_Irrlicht::render()
--- source/irrlicht/SpriteNode_Irrlicht.hpp
+++ source/irrlicht/SpriteNode_Irrlicht.hpp
@@ -14,69 +14,34 @@ using namespace irr;
 class SpriteNode_Irrlicht :  public irr::scene::ISceneNode	
 {
 public:    
-	explicit SpriteNode_Irrlicht(scene::ISceneNode* parent, scene::ISceneManager* mgr, s32 id,
-		unsigned frameTime = 200.f, bool paused = false, bool looped = true);
+	explicit SpriteNode_Irrlicht(scene::ISceneNode* parent, scene::ISceneManager* mgr, s32 id);
 
 	virtual ~SpriteNode_Irrlicht();
 
-    void update(unsigned deltaTime);
-	void setFrameTime(unsigned time);
-	unsigned getFrameTime() const;
-
-    void setAnimation(const SpriteIrrlicht& animation);
-	void setAnimation(const std::string& animationName);
-    void play();
-    void play(const SpriteIrrlicht& animation);
-    void pause();
-    void stop();
-    void setLooped(bool looped);	
-    void setColor(const video::SColor& color);
-    const SpriteIrrlicht* getAnimation() const;
-
 	irr::core::rectf getLocalBounds() const;
 	irr::core::rectf getGlobalBounds() const;
+	void setColor(const video::SColor& color);
 
-    bool isLooped() const;
-    bool isPlaying() const;
-    
-    void setFrame(std::size_t newFrame, bool resetTime = true);
+	void set_texture(irr::video::ITexture *texture);
 
+	void setFrame(std::size_t frameIndex, const SpriteIrrlicht* sprite);
 
-	virtual s32 getMaterialCount()
-	{
-		return 1;
-	}
+	virtual s32 getMaterialCount(){ return 1; }
 
-	virtual video::SMaterial& getMaterial(s32 i)
-	{
-		return m_material;
-	} 
+	virtual video::SMaterial& getMaterial(s32 i){ return m_material; } 
 	virtual void OnRegisterSceneNode() override;
 
 	virtual void render() override;
 
 	virtual const core::aabbox3d<f32>& getBoundingBox() const override;
 
-	inline bool getFlipped() const { return m_isFlipped; }
-	inline void setFlipped(bool val) { m_isFlipped = val; }
-
-private:
-    const SpriteIrrlicht* m_sprite;
-
+private:    
+    
 	core::aabbox3d<f32> m_box;
-
-	unsigned			m_frameTime;
-	unsigned			m_currentTime;
-
-    std::size_t			m_currentFrame;
-    bool				m_isPaused;
-    bool				m_isLooped;
-    
-	video::ITexture*	m_texture;
-    
+	video::ITexture*	m_texture;    
 	video::S3DVertex	m_vertices[4];
 	u16					m_indices[6];
 	video::SMaterial	m_material;
-
-	bool				m_isFlipped;	
+	irr::core::recti	m_frame_rect;
+	
 };
