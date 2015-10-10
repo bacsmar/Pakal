7c1c832f8691e139c51861ec1eb3156b4d1e2b91
SNRG <SNRG@deab2f4f-25c9-4a44-aba5-3c43511eb6ab>
2015-10-10 21:54:08 +0000

    + Factory.h

---
+++ trunk/libs/Pakal_II/source/Factory.h
@@ -0,0 +1,42 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2015 The PakalEngine Open Source Project
+// File: IComponentFactory.h
+// Original Author: Salvador Noel Romo Garcia.
+// Creation Date: 01-11-2012
+//
+// Purpose: Interface to create Icomponent
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+#include "config.h"
+#include <functional>
+
+
+namespace Pakal
+{
+	///////////////////////////////////////////////////////////////////////////////////////////////////////
+	template <class T>
+	class IFactory
+	{
+	public:
+		std::function<T*()> create;
+
+		virtual const char* get_typename() = 0;
+
+		virtual ~IFactory() {}
+	};
+	///////////////////////////////////////////////////////////////////////////////////////////////////////
+	template <class TRoot, class TBase>
+	inline IFactory<TRoot>* CreateFactory(const std::function<TRoot*()>& _fn)
+	{
+		static_assert(std::is_base_of < TRoot, TBase>::value, "Incompatible Types");
+		struct Factory : IFactory<TRoot>
+		{
+			virtual const char* get_typename() override { return TypeInfo::get<TBase>().getName(); };
+			explicit Factory(const std::function<TRoot*()>& fn) { create = fn; }
+		};
+		return new Factory(_fn);
+	}	
+	///////////////////////////////////////////////////////////////////////////////////////////////////////
+}
