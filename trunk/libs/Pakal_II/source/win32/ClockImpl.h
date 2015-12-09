a2050064f07845cc287424a783c7a3ac34ced9b9
SNRG <SNRG@deab2f4f-25c9-4a44-aba5-3c43511eb6ab>
2015-12-09 02:57:20 +0000

    * ClockImpl windows 10

--- trunk/libs/Pakal_II/source/win32/ClockImpl.h
+++ trunk/libs/Pakal_II/source/win32/ClockImpl.h
@@ -3,19 +3,60 @@
 #include "Config.h"
 #include "PakalTime.h"
 #include <chrono>
+#include <windows.h>
+#include <time.h>
 
 
 namespace Pakal
 {
 namespace priv
 {
+	struct ClockInitializer
+	{
+		LARGE_INTEGER HighPerformanceFreq;
+		BOOL HighPerformanceTimerSupport = true;
+		BOOL MultiCore = false;
+		bool usePerformanceTimer = true;
+
+		ClockInitializer()
+		{
+			SYSTEM_INFO sysinfo;
+			GetSystemInfo(&sysinfo);
+			MultiCore = (sysinfo.dwNumberOfProcessors > 1);
+			if (usePerformanceTimer)
+				HighPerformanceTimerSupport = QueryPerformanceFrequency(&HighPerformanceFreq);
+			else
+				HighPerformanceTimerSupport = FALSE;			
+		}
+	};
 class ClockImpl
-{
+{	
 public:
     static Time getCurrentTime()
     {
-		auto currenttime = std::chrono::system_clock::now();
-		return Pakal::microseconds(std::chrono::duration_cast< std::chrono::microseconds >(currenttime.time_since_epoch()).count());
+		static ClockInitializer clock;		
+
+		// based on irrlicht timer.
+		if (clock.HighPerformanceTimerSupport)
+		{
+			// Avoid potential timing inaccuracies across multiple cores by
+			// temporarily setting the affinity of this process to one core.
+			DWORD_PTR affinityMask = 0;
+			if (clock.MultiCore)
+				affinityMask = SetThreadAffinityMask(GetCurrentThread(), 1);
+			LARGE_INTEGER nTime;
+			BOOL queriedOK = QueryPerformanceCounter(&nTime);
+
+
+			// Restore the true affinity.
+			if (clock.MultiCore)
+				(void)SetThreadAffinityMask(GetCurrentThread(), affinityMask);
+			if (queriedOK)
+				return Pakal::microseconds(unsigned((nTime.QuadPart) * 1000 / clock.HighPerformanceFreq.QuadPart) * 1000);
+
+		}
+
+		return Pakal::microseconds(GetTickCount()*1000);
     }
 };
 
