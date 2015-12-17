d28e63c3a2d0644f69f5539f65ad488814a3507d
elios264 <elios264@deab2f4f-25c9-4a44-aba5-3c43511eb6ab>
2015-12-17 17:12:12 +0000

    uimanager format

--- source/IUIManager.h
+++ source/IUIManager.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #include "Config.h"
-#include "IManager.h"
+#include "PakalPath.h"
 #include "IComponentProvider.h"
-#include "InputDevice.h"
+
 
 namespace Pakal
 {
@@ -11,12 +11,15 @@ namespace Pakal
 	{
 	public:
 		virtual ~IUIManager() {};
-		virtual bool load_document(const char* documentPath, bool show = true, bool autoresize = true) = 0;
-		virtual bool close_document(const char* documentId) = 0;
-		virtual bool set_document_visible(const char* documentId, const bool visible) = 0;
-		virtual bool load_font(const char* path) = 0;
 
-		virtual void update_ui(long long dt) = 0;
+		virtual bool load_document(unsigned id, const Path& resourcePath) = 0;
+		virtual bool unload_document(unsigned id) = 0;
+
+		virtual void display_document(unsigned id, bool autoresize = true) = 0;
+		virtual void conceal_document(unsigned id) = 0;
+
+		virtual bool load_font(const Path& resourcePath) = 0;
+
 		virtual void draw_ui() = 0;
 		virtual void initialize() = 0;
 		virtual void terminate() = 0;
