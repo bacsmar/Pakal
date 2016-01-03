7309f44b63a09797b9a8f7e051c42cbe9d84cfc0
SNRG <SNRG@deab2f4f-25c9-4a44-aba5-3c43511eb6ab>
2016-01-03 22:58:01 +0000

    + adding PlayerVsPlayerState * adding new proxies for LuaProxies.cpp

--- source/LuaProxies.cpp
+++ source/LuaProxies.cpp
@@ -1,33 +1,40 @@
 #include "LuaProxies.h"
 
 #if PAKAL_USE_SCRIPTS == 1
-
+/////////////////////////////////////////////////////////////
 OOLUA_EXPORT_FUNCTIONS(SpriteComponent, set_animation, set_flipped)
 OOLUA_EXPORT_FUNCTIONS_CONST(SpriteComponent,is_playing)
-
+/////////////////////////////////////////////////////////////
 OOLUA_EXPORT_FUNCTIONS(SFXComponent, add, play)
 EXPORT_OOLUA_FUNCTIONS_0_CONST(SFXComponent)
-
+/////////////////////////////////////////////////////////////
 OOLUA_EXPORT_FUNCTIONS(GamepadComponent, is_button_pressed)
 //OOLUA_EXPORT_FUNCTIONS_CONST(GamepadComponent)
 //OOLUA_EXPORT_FUNCTIONS(Enums, set_enum, get_enum)
 EXPORT_OOLUA_FUNCTIONS_0_CONST(GamepadComponent)
-
+/////////////////////////////////////////////////////////////
 OOLUA_EXPORT_FUNCTIONS(Pakal::tmath::vector2df)
 OOLUA_EXPORT_FUNCTIONS_CONST(Pakal::tmath::vector2df, get_x, get_y)
-
+/////////////////////////////////////////////////////////////
 OOLUA_EXPORT_FUNCTIONS(Pakal::SpritebodyComponent, apply_impulse, set_lineal_velocity, apply_force)
 OOLUA_EXPORT_FUNCTIONS_CONST(Pakal::SpritebodyComponent, get_lineal_velocity)
-
+/////////////////////////////////////////////////////////////
 //	LUA, definicion
 OOLUA_EXPORT_FUNCTIONS(Pakal::LogMgr, log)
 EXPORT_OOLUA_FUNCTIONS_0_CONST(Pakal::LogMgr)
+
+OOLUA_EXPORT_FUNCTIONS(Path)
+EXPORT_OOLUA_FUNCTIONS_0_CONST(Path)
+
+OOLUA_EXPORT_FUNCTIONS(UIManager, load_document, unload_document, show_document, hide_document)
+EXPORT_OOLUA_FUNCTIONS_0_CONST(UIManager)
 
 namespace Pakal
 {
 	template <>
 	void oolua_script_interface_for<SpriteComponent>(SpriteComponent* obj, OOLUA::Script& script, const std::string& name)
 	{
+		ASSERT(name.size() > 0);
 		script.register_class<SpriteComponent>();
 		
 		auto result = OOLUA::push(script.state(), obj, OOLUA::Cpp);
@@ -39,6 +46,7 @@ namespace Pakal
 	template <>
 	void oolua_script_interface_for<SpritebodyComponent>(SpritebodyComponent* obj, OOLUA::Script& script, const std::string& name)
 	{
+		ASSERT(name.size() > 0);
 		script.register_class<SpritebodyComponent>();
 
 		auto result = OOLUA::push(script.state(), obj, OOLUA::Cpp);
@@ -50,6 +58,7 @@ namespace Pakal
 	template <>
 	void oolua_script_interface_for<GamepadComponent>(GamepadComponent* obj, OOLUA::Script& script, const std::string& name)
 	{
+		ASSERT(name.size() > 0);
 		script.register_class<GamepadComponent>();
 
 		auto result = OOLUA::push(script.state(), obj, OOLUA::Cpp);
@@ -61,6 +70,7 @@ namespace Pakal
 	template <>
 	void oolua_script_interface_for<SFXComponent>(SFXComponent* obj, OOLUA::Script& script, const std::string& name)
 	{
+		ASSERT(name.size() > 0);
 		script.register_class<SFXComponent>();
 
 		auto result = OOLUA::push(script.state(), obj, OOLUA::Cpp);
---
+++ source/script/ScriptableGameState.cpp
@@ -0,0 +1,54 @@
+#include "ScriptableGameState.h"
+
+#if PAKAL_USE_SCRIPTS == 1
+#include "Components/ScriptComponent.h"
+#include "ResourceManager.h"
+#include "Engine.h"
+
+Pakal::ScriptableGameState::ScriptableGameState(const std::string& name, const std::string& script_file): BaseGameState(name), m_script_file(script_file)
+{	
+}
+
+Pakal::ScriptableGameState::~ScriptableGameState()
+{	
+}
+
+void Pakal::ScriptableGameState::on_initialize(Engine* engine)
+{
+	auto resource = ResourceMgr.open_read_resource(m_script_file);
+
+	ASSERT(resource->good());
+
+	std::string script;
+	file_utils::read_to_string(*resource, script);
+
+	m_script = new ScriptComponent();
+
+	m_script->load_script(script);
+	m_script->register_on_script(engine->get_ui_manager(), "UI");
+
+	m_script->call_function("on_initialize");
+}
+
+void Pakal::ScriptableGameState::on_terminate(Engine* engine)
+{
+	m_script->call_function("on_terminate");
+	SAFE_DEL(m_script);
+}
+
+void Pakal::ScriptableGameState::on_update()
+{
+	m_script->call_function("on_update");
+}
+
+void Pakal::ScriptableGameState::on_activate(Engine* engine)
+{
+	m_script->call_function("on_activate");
+}
+
+void Pakal::ScriptableGameState::on_deactivate(Engine* engine)
+{
+	m_script->call_function("on_deactivate");
+}
+
+#endif //PAKAL_USE_SCRIPTS
