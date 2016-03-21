1f034bb9bf530ee3f609aebc6dc13a636c848b4a
SNRG <SNRG@deab2f4f-25c9-4a44-aba5-3c43511eb6ab>
2016-03-21 01:40:53 +0000

    * minor improvements to InputHandlerComponent.h

--- source/components/InputHandlerComponent.cpp
+++ source/components/InputHandlerComponent.cpp
@@ -3,6 +3,7 @@
 #include <persist/TextReader.h>
 #include "ICharacterHandler.h"
 #include "LogMgr.h"
+#include <algorithm>
 
 
 Pakal::KeyboardHandlerComponent::KeyboardHandlerComponent(Pakal::IInputManager* inputManager) : m_input_manager_ref(inputManager), m_has_linked_components(false)
@@ -14,7 +15,7 @@ Pakal::KeyboardHandlerComponent::KeyboardHandlerComponent(Pakal::IInputManager* 
 	m_id_event_key_up = m_input_manager_ref->event_key_up.add_listener([=](const Pakal::KeyArgs& args)
 	{
 		this->traslate_command_kUp(args);
-	}, THIS_THREAD); 
+	}, THIS_THREAD);
 
 }
 
@@ -23,28 +24,30 @@ Pakal::KeyboardHandlerComponent::~KeyboardHandlerComponent()
 	m_input_manager_ref->event_key_up.remove_listener(m_id_event_key_up);
 	m_input_manager_ref->event_key_down.remove_listener(m_id_event_key_down);
 	m_input_manager_ref = nullptr;
-	if (true == m_has_linked_components) 
-	{		
-		m_handled_component->unsuscribe(this);		
+	if (true == m_has_linked_components)
+	{
+		m_handled_component->unsuscribe(this);
 	}
 	m_handled_component = nullptr;
 }
 
 void Pakal::KeyboardHandlerComponent::set_and_load_character_handler(ICharacterHandler* handledcomponent)
 {
-	ASSERT_MSG(m_has_linked_components == false, "input handler already links a CharacterDriver, to make a link again please use remove_subscriptions() ");	
+	ASSERT_MSG(m_has_linked_components == false, "input handler already links a CharacterDriver, to make a link again please use remove_subscriptions() ");
 
 	LOG_INFO("[input handler] loading mapping for: %s", handledcomponent->get_character_handler_typename().c_str());
 
-	this->m_handled_component = handledcomponent;	
+	this->m_handled_component = handledcomponent;
 
-	this->m_key_map.push_front({ Pakal::Key::Left,"WALK_LEFT" , KeyMapping::KeyEvent::DOWN });
-	this->m_key_map.push_front({ Pakal::Key::Right,"WALK_RIGHT", KeyMapping::KeyEvent::DOWN });
-	this->m_key_map.push_front({ Pakal::Key::Down,"STAND_BY", KeyMapping::KeyEvent::DOWN });
-	this->m_key_map.push_front({ Pakal::Key::Up,"JUMP", KeyMapping::KeyEvent::DOWN });
+	// TODO: implement load_mapping
 
-	this->m_key_map.push_front({ Pakal::Key::Left,"STOP_WL" , KeyMapping::KeyEvent::UP });
-	this->m_key_map.push_front({ Pakal::Key::Right,"STOP_WR", KeyMapping::KeyEvent::UP });
+	this->m_key_map.push_front({ Pakal::Key::Left,"WALK_LEFT" , KeyMapping::Type::DOWN });
+	this->m_key_map.push_front({ Pakal::Key::Right,"WALK_RIGHT", KeyMapping::Type::DOWN });
+	this->m_key_map.push_front({ Pakal::Key::Down,"STAND_BY", KeyMapping::Type::DOWN });
+	this->m_key_map.push_front({ Pakal::Key::Up,"JUMP", KeyMapping::Type::DOWN });
+
+	this->m_key_map.push_front({ Pakal::Key::Left,"STOP_WL" , KeyMapping::Type::UP });
+	this->m_key_map.push_front({ Pakal::Key::Right,"STOP_WR", KeyMapping::Type::UP });
 
 	m_has_linked_components = m_handled_component->suscribe_to_input_handler(this);
 }
@@ -66,30 +69,29 @@ bool Pakal::KeyboardHandlerComponent::has_linked_components() const
 
 void Pakal::KeyboardHandlerComponent::traslate_command_kdown(Pakal::KeyArgs args)
 {
-	if (true == this->settings.use_key_down)//TODO:check for m_key_map size
+	auto result = std::find_if(m_key_map.begin(), m_key_map.end(), [=](const KeyMapping& mapping) -> bool 
 	{
-		for (auto& keyValue : m_key_map)
-		{
-			if (keyValue.key == args.key && keyValue.event == KeyMapping::KeyEvent::DOWN)
-				this->evt_translated_command.notify(keyValue.command);
-		}
-	}
+		return mapping.key == args.key && mapping.event_type == KeyMapping::Type::DOWN;
+	});
+
+	if (result != m_key_map.end())
+		evt_translated_command.notify(result->command);
 }
 
 void Pakal::KeyboardHandlerComponent::traslate_command_kUp(Pakal::KeyArgs args)
 {
-	if (true== this->settings.use_key_up)//TODO:check for m_key_map size 
+	auto result = std::find_if(m_key_map.begin(), m_key_map.end(), [=](const KeyMapping& mapping) -> bool
 	{
-		for(auto& keyValue: m_key_map)
-		{
-			if(keyValue.key==args.key && keyValue.event== KeyMapping::KeyEvent::UP)
-				this->evt_translated_command.notify(keyValue.command);
-		}
-	}
+		return mapping.key == args.key && mapping.event_type == KeyMapping::Type::UP;
+	});
+
+	if (result != m_key_map.end())
+		evt_translated_command.notify(result->command);
 }
 
 bool Pakal::KeyboardHandlerComponent::load_mapping(Pakal::TextReader& reader, std::istream& stream)
 {
+	// TODO: implement it
 	ASSERT(false);
 	//auto result = reader.read(stream, "SpriteSheetAnimation", *this);
 	//ASSERT(m_ref_size.get_length() > 0); 
