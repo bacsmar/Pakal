473fb2f2a4f9e55af52380a9a857c6ec59a3738f
SNRG <SNRG@deab2f4f-25c9-4a44-aba5-3c43511eb6ab>
2016-04-08 02:27:18 +0000

    * adding UI sample

--- trunk/libs/Pakal_II/source/rocketgui/RocketUIComponents.h
+++ trunk/libs/Pakal_II/source/rocketgui/RocketUIComponents.h
@@ -12,8 +12,10 @@ namespace Pakal
 
 		void set_ui_control(unsigned int documentId, const std::string& controlName) override;
 		void set_text(const std::string& text) override;
+		void set_visible(bool visible) override;
 	private:
 		RocketUI* m_rocket_ui = nullptr;
+		Rocket::Core::Element* m_element = nullptr;
 		unsigned m_document_id = 0;
 		std::string m_control_name;
 	};
